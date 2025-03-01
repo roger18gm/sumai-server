37eafca5fad23bbb818cadbfc57148b8b675e6ea
rawwyurr <roger18gm@gmail.com>
2025-03-01 12:19:41 -0700

    host

--- application.py
+++ application.py
@@ -113,4 +113,4 @@ def chat_stream():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    application.run(debug=True, port=5000)
+    application.run(host="0.0.0.0", port=80)
